09a337f27088faf91a1269a732dee11f764073a3
skgm1992 <shubhj.1588@gmail.com>
2026-01-28 16:47:08 +0530

    live stream for project

---
+++ config/config.js
@@ -0,0 +1,13 @@
+const PORT = 3000;
+const DB_URL = "mongodb+srv://shubhj1588_db_user:DHqNlEf4AVUariXx@cluster0.rpsk8i3.mongodb.net/";
+const ENC_KEY = "the stepping stone";
+const DOMAIN = "http://localhost:3000";
+const DOMAIN_FRONT = "http://localhost:5173";
+
+const RAZORPAY_KEY = "rzp_test_Rek8z2OtrReaiV";
+const RAZORPAY_SECRET = "c3QZBCHSmaMmuLK9Gc9y1MtK";
+
+export {PORT, DB_URL, ENC_KEY, DOMAIN, DOMAIN_FRONT, RAZORPAY_KEY, RAZORPAY_SECRET}
+
+// key rzp_test_Rek8z2OtrReaiV
+// secret c3QZBCHSmaMmuLK9Gc9y1MtK
